4e7b95d2bfe4e4a66b1b8040c37aa683dbbf2063
doushenwei <doushenwei@163.com>
2018-05-07 16:30:31 +0800

    Update SampleEula.java

--- src/android/com/huawei/mdm/sample/SampleEula.java
+++ src/android/com/huawei/mdm/sample/SampleEula.java
@@ -1,5 +1,5 @@
 /*
- * IMPORTANT:  This Huawei software is supplied to you by Huawei Technologies Co., Ltd. 
+ * IMPORTANT:  This Huawei software is supplied to you by Huawei Technologies Co., Ltd.
  * ("Huawei") in consideration of your agreement to the following
  * terms, and your use, copy, installation, modification or redistribution of
  * this Huawei software constitutes acceptance of these terms.  If you do
@@ -8,7 +8,7 @@
 
  * In consideration of your agreement to abide by the following terms, and
  * subject to these terms, Huawei grants you a personal, non-exclusive
- * license, under Huawei's copyrights in this original Huawei software(hereinafter referred as ¡°Huawei Software¡±), to use, reproduce, modify and redistribute the Huawei Software, with or without modifications, in source and/or binary forms;
+ * license, under Huawei's copyrights in this original Huawei software(hereinafter referred as ï¿½ï¿½Huawei Softwareï¿½ï¿½), to use, reproduce, modify and redistribute the Huawei Software, with or without modifications, in source and/or binary forms;
  * provided that if you redistribute the Huawei Software in its entirety and
  * without modifications, you must retain this notice and the following
  * text and disclaimers in all such redistributions of the Huawei Software.
@@ -41,7 +41,7 @@
  * Huawei and other Huawei trademarks are trademarks of Huawei Technologies Co., Ltd.
  * All other trademarks and trade names mentioned in this document are the property of their respective holders.
  */
-package com.huawei.mdm.sample;
+package cordova.plugin.huaweiswatchapn;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -65,6 +65,8 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.TextView;
+
+import com.chinavvv.jwtoa.R;
 
 public class SampleEula {
     private static final int REQUEST_ENABLE = 1;
@@ -111,7 +113,7 @@ public class SampleEula {
 
                             });
             AlertDialog eulaDialog = builder.create();
-            eulaDialog.setCancelable(false); 
+            eulaDialog.setCancelable(false);
             LayoutInflater inflater = LayoutInflater.from(mActivity);
             View layout = inflater.inflate(R.layout.eula_layout, null);
             TextView permissionText = (TextView)layout.findViewById(R.id.content_permissions);
@@ -120,23 +122,23 @@ public class SampleEula {
             permissionText.setText(Html.fromHtml(content));
 
             TextView statementText = (TextView)layout.findViewById(R.id.read_statement);
-            statementText.setMovementMethod(LinkMovementMethod.getInstance());  
-            CharSequence text = statementText.getText();  
-            if (text instanceof Spannable) {  
-                int end = text.length();  
+            statementText.setMovementMethod(LinkMovementMethod.getInstance());
+            CharSequence text = statementText.getText();
+            if (text instanceof Spannable) {
+                int end = text.length();
                 Spannable sp = (Spannable) text;
-                URLSpan[] urls = sp.getSpans(0, end, URLSpan.class);  
-                SpannableStringBuilder style = new SpannableStringBuilder(text);  
-                style.clearSpans();// should clear old spans  
-                for (URLSpan url : urls) {  
-                    MyURLSpan myURLSpan = new MyURLSpan();  
-                    style.setSpan(myURLSpan, sp.getSpanStart(url), sp.getSpanEnd(url), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);  
-                }  
-                statementText.setText(style);  
+                URLSpan[] urls = sp.getSpans(0, end, URLSpan.class);
+                SpannableStringBuilder style = new SpannableStringBuilder(text);
+                style.clearSpans();// should clear old spans
+                for (URLSpan url : urls) {
+                    MyURLSpan myURLSpan = new MyURLSpan();
+                    style.setSpan(myURLSpan, sp.getSpanStart(url), sp.getSpanEnd(url), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
+                }
+                statementText.setText(style);
             }
             CheckBox checkbox = (CheckBox)layout.findViewById(R.id.not_show_check);
             checkbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-                
+
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     if(isChecked){
@@ -144,7 +146,7 @@ public class SampleEula {
                     } else {
                         notShowAgain = false;
                     }
-                    
+
                 }
             });
             eulaDialog.setView(layout);
@@ -154,15 +156,15 @@ public class SampleEula {
         }
     }
 
-    private class MyURLSpan extends ClickableSpan {  
-        @Override  
-        public void onClick(View widget) {  
-            widget.setBackgroundColor(Color.parseColor("#00000000"));  
-  
-            Intent intent = new Intent(mActivity, LicenseActivity.class);  
-            mActivity.startActivity(intent);  
+    private class MyURLSpan extends ClickableSpan {
+        @Override
+        public void onClick(View widget) {
+            widget.setBackgroundColor(Color.parseColor("#00000000"));
+
+            Intent intent = new Intent(mActivity, LicenseActivity.class);
+            mActivity.startActivity(intent);
         }
-    }  
+    }
 
     protected void activeProcess() {
         if (mDevicePolicyManager != null
