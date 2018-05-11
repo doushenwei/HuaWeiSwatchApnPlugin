ab768c9968dcd0fd3fa60b2087624d77fe647275
doushenwei <doushenwei@163.com>
2018-05-11 11:42:06 +0800

    Update HuaWeiSwitchApnPlugin.js

--- www/HuaWeiSwitchApnPlugin.js
+++ www/HuaWeiSwitchApnPlugin.js
@@ -3,3 +3,9 @@ var exec = require('cordova/exec');
 exports.setApn = function (arg0, success, error) {
     exec(success, error, 'HuaWeiSwitchApnPlugin', 'setApn', [arg0]);
 };
+exports.setInternet = function (arg0, success, error) {
+    exec(success, error, 'HuaWeiSwitchApnPlugin', 'setInternet', [arg0]);
+};
+exports.initApnConfig = function (arg0, success, error) {
+    exec(success, error, 'HuaWeiSwitchApnPlugin', 'initApnConfig', [arg0]);
+};
